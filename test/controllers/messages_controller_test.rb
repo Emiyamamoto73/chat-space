71d1540b19d4f46491aec93c5522cb41f5fe2d2c
Emi Yamamoto <emi.yamamoto73@gmail.com>
2019-05-17 18:56:26 +0900

    set up for beginning and revised view

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MessagesControllerControllerTest < ActionDispatch::IntegrationTest
+class MessagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
