57cf110ec19322eeed5142e6919a29f0d8d73f63
Emi Yamamoto <emi.yamamoto73@gmail.com>
2019-06-06 15:39:56 +0900

    add automatic-update

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,8 @@
 $(function(){
   function buildHTML(message){
+    // console.log(message)
     var image = message.image ? `<img src= ${message.image}>` : "";
-    var html = `<div class="message">
+    var html = `<div class="message" data-id = ${message.id}>
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${message.user_name}
@@ -19,6 +20,28 @@ $(function(){
                 </div>`
   return html;
   }
+
+  function insertHTML(message){
+    var image = message.image ? `<img src= ${message.image}>` : "";
+    var content = message.content ? `${message.content}` : "";
+    var html = `<div class="message" data-id = ${message.id}>
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                    ${content}
+                  </p>
+                    ${image}
+                </div>
+              </div>`
+  return html;
+  }
   
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -34,11 +57,10 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
+      // console.log(data)
       $('.messages').append(html);
       $("form")[0].reset();
-      $('.messages').delay(100).animate({
-        scrollTop: $(document).height()
-      },1500);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .always(() => {
       $(".form__submit").removeAttr("disabled");
@@ -47,4 +69,32 @@ $(function(){
       alert('エラーが発生したためメッセージは送信できませんでした。');
     })
   })
+
+
+  $(function() {
+    var reloadMessages = function() {
+      last_message_id = $(".message").last().data("id");  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      $.ajax({
+        url: 'api/messages',  //ルーティングで設定した通りのURLを指定
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}   //dataオプションでリクエストに値を含める
+      })
+      .done(function(messages) {
+        // console.log(messages)
+          messages.forEach(function(message){
+            var html = insertHTML(message);
+            $(".messages").append(html);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          })
+        
+        
+      })
+      .fail(function () {
+        // console.log('error')
+      });
+     }
+     setInterval(reloadMessages, 5000);
+    })
+      
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,11 @@
-json.id      @message.id
-json.content @message.content 
+# json.id      @message.id
+# json.content @message.content 
 json.date    @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.user_name @message.user.name
+# json.user_name @message.user.name
 json.image   @message.image.url
+
+json.(@message, :content)
+# json.created_at @message.created_at
+json.user_name @message.user.name
+#idもデータとして渡す
+json.id @message.id
